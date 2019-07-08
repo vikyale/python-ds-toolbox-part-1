39d3d8309b7e80f8d026c6f24aeac2b5458dc432
Victoria Ubaldo <viky.ubaldo@gmail.com>
2019-07-08 18:49:49 -0500

    first commit

---
+++ 1.Writing your own functions/introduction-to-tuples.py
@@ -0,0 +1,12 @@
+"""
+Construct a new tuple, even_nums composed 
+of the same elements in nums, but with the 1st element 
+replaced with the value, 2.
+"""
+
+
+# Unpack nums into num1, num2, and num3
+num1, num2, num3 = nums
+
+# Construct even_nums
+even_nums = (2, num2, num3)
